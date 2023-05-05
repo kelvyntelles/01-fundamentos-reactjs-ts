62a310e561d4d2e1c5753637c117a2c780da3f35
kelvyntelles <kelvyn.telles@gmail.com>
2023-05-05 17:53:32 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { Header } from './components/Header';
+import { Post, PostType } from "./components/Post"
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+
+import './global.css';
+
+const posts: PostType[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto. É um projeto que fiz com react JS.' },
+      { type: 'link', content: 'jane.design/doctore' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto. É um projeto que fiz com react JS.' },
+      { type: 'link', content: 'jane.design/doctore' },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00'),
+  },
+];
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                post={post}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
